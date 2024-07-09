9e8679a44f9cd4c86b56343b6e58586441e7dd21
zhaleh <zhalehelmi@gmail.com>
2024-07-09 13:08:08 +0330

    cardetails complete

--- components/carCard/index.tsx
+++ components/carCard/index.tsx
@@ -41,7 +41,7 @@ const CarCard = ({ car }: CarCardProps) => {
       </div>
 
       <div className="relative flex w-full mt-2">
-        <div className="flex group-hover:invisible w-full justify-between text-gray">
+        <div className="flex group-hover:invisible w-full justify-between text-grey">
           <div className="flex flex-col justify-center items-center gap-2">
             <Image
               src="/steering-wheel.svg"
